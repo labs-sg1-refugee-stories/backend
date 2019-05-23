7506bbe54055822c8a60f203ad7f1b4a0e2089c7
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-05-23 14:59:07 -0700

    Made changes in dbConfif file

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,8 +1,8 @@
 const knex = require("knex");
 const knexConfig = require("../knexfile.js");
 
-// const db = knex(knexConfig.development);
-// module.exports = db;
+const db = knex(knexConfig.development);
+module.exports = db;
 
-const dbEnv = process.env.DB_ENV || development;
-module.exports = knex(knexConfig[dbEnv]);
+// const dbEnv = process.env.DB_ENV || development;
+// module.exports = knex(knexConfig[dbEnv]);
