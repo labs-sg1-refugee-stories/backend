e89504b257e1113c45026fb9eb2517e1e5e6b34c
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-05-22 18:19:23 -0700

    Tested the server with Supertest

---
+++ api/server.spec.js
@@ -0,0 +1,33 @@
+const request = require("supertest");
+const server = require("../api/server.js");
+
+describe("Basic server tests", () => {
+  it("Sets the environment to testing", () => {
+    expect(process.env.DB_ENV).toBe("testing");
+  });
+
+  // open client, make a request and inspect the response
+  describe("GET /", () => {
+    it("should return 200 OK", () => {
+      // we return the promise
+      return request(server)
+        .get("/")
+        .expect(200);
+    });
+
+    it("Using the squad (async/await)", async () => {
+      // use the squad
+      const res = await request(server).get("/");
+      expect(res.status).toBe(200);
+    });
+    it("Should return JSON using done callback", done => {
+      // using the done callback
+      request(server)
+        .get("/")
+        .then(res => {
+          expect(res.type).toBe("text/html"); // Content-Type
+          done();
+        });
+    });
+  });
+});
