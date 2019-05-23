a6a8d1d3ed830b44488710405844254dab685fa7
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-05-22 18:03:05 -0700

    Installed dependencies for server testing

--- api/server.spec.js
+++
@@ -1,9 +0,0 @@
-// const request = require("supertest");
-// const server = require("../api/server.js");
-
-// describe("Basic server tests", () => {
-//   it("Should respond with index.html for root GET", async () => {
-//     const res = await request(server).get("/");
-//     expect(res.type).toBe("text/html");
-//   });
-// });
