94484a154724dcd80e75e7fdb2d39b8fa27fea1d
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-05-24 19:31:54 -0700

    adds .prettierrc to help spark joy in the code base ✨

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
-const knex = require('knex')
-const knexConfig = require('../knexfile.js')
+require('dotenv').config();
+const knex = require('knex');
+const knexConfig = require('../knexfile.js');
 
-const db = knex(knexConfig[process.env.NODE_ENV])
-module.exports = db
+const db = knex(knexConfig[process.env.NODE_ENV]);
+module.exports = db;
 
 // const dbEnv = process.env.DB_ENV || development;
 // module.exports = knex(knexConfig[dbEnv]);
--- knexfile.js
+++ knexfile.js
@@ -19,7 +19,7 @@ module.exports = {
     },
     pool: {
       afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done)
+        conn.run('PRAGMA foreign_keys = ON', done);
       },
     },
     migrations: {
@@ -39,4 +39,4 @@ module.exports = {
       directory: './database/seeds',
     },
   },
-}
+};
