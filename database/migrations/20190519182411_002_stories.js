e9c5b54c5c1a144314072d9cd9fe95211dabcef6
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-06-05 19:27:31 -0700

    adds migration to add timestamps to stories and pending stories

--- database/migrations/20190519182411_002_stories.js
+++ database/migrations/20190519182411_002_stories.js
@@ -1,24 +1,23 @@
 exports.up = function(knex, Promise) {
   return knex.schema
-    .createTable("stories", stories => {
+    .createTable('stories', stories => {
       stories.increments();
-
-      stories.string("title", 255).notNullable();
-      stories.string("name", 128).notNullable();
-      stories.text("storytext", 255).notNullable();
-      stories.string("country", 255);
+      stories.string('title', 255).notNullable();
+      stories.string('name', 128).notNullable();
+      stories.text('storytext', 255).notNullable();
+      stories.string('country', 255);
     })
-    .createTable("pending_stories", pending => {
+    .createTable('pending_stories', pending => {
       pending.increments();
-      pending.string("title");
-      pending.string("name");
-      pending.text("storytext");
-      pending.string("country");
+      pending.string('title');
+      pending.string('name');
+      pending.text('storytext');
+      pending.string('country');
     });
 };
 
 exports.down = function(knex, Promise) {
   return knex.schema
-    .dropTableIfExists("stories")
-    .dropTableIfExists("pending_stories");
+    .dropTableIfExists('stories')
+    .dropTableIfExists('pending_stories');
 };
